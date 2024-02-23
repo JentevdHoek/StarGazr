0e576aebaf0160c023c210fa22eba54a8cdf0a01
JentevdHoek <jentevanderhoek@gmail.com>
2024-02-23 10:55:24 +0100

    Initial Commit

---
+++ StarGazr/StarGazrApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarGazrApp.swift
+//  StarGazr
+//
+//  Created by Jente van der Hoek on 23/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct StarGazrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
